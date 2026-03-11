be1b9b55a82bc45fcd751797adfbadc32a6ad1ab
Manoj Chille <manoj.chille.web@gmail.com>
2026-03-11 13:36:35 +0530

    Initial dashboard micro frontend

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import { CustomResourceLoader } from '@nguniversal/common/clover/server/src/custom-resource-loader';
+import { createFetch } from '@angular-architects/module-federation/nguniversal';
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
@@ -41,9 +43,7 @@ app.use(
 app.use((req, res, next) => {
   angularApp
     .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
+    .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next);
 });
 
